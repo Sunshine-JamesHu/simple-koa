7ceeb7881de2b10dcf055c68c8df3e031206a4b8
Sunshine-JamesHu <690531347@qq.com>
2021-12-07 10:58:22 +0800

    feat:升级版本到1.1.7 解决缺少.d.ts的问题

--- index.ts
+++ index.ts
@@ -1,32 +1,50 @@
-import { Transient, Singleton, Injectable, Inject } from './src/di/Dependency';
+import { Transient, Singleton, Injectable, Inject, GetInjectInfo, Container, Scoped, ReplaceService, AllowMultiple } from './src/di/Dependency';
 import { ISettingManager, SettingManager } from './src/setting/SettingManager';
 import { Router } from './src/router/Router';
 import { RequestBody, RequestQuery } from './src/router/RequestData';
 import { HttpRequest, HttpGet, HttpDelete, HttpPut, HttpOptions, HttpPost } from './src/router/Request';
 import { IService, Service } from './src/service/Service';
 import { IController, Controller } from './src/controller/Controller';
+import { EventHandler, EventKey, IEventHandler } from './src/event/EventHandler';
+import { IQueueManager, QueueManager } from './src/queue/QueueManager';
+import { IQueueManagerFactory, QueueManagerFactory } from './src/queue/QueueManagerFactory';
+
 import Program from './src/Program'; // 这一句必须放在最后面,否则打出来的包就会有问题
+import { ArrayHelper } from './src/tools/ArrayHelper';
 
 export {
-  Transient,
+  Container,
   Singleton,
+  Transient,
+  Scoped,
   Injectable,
   Inject,
+  GetInjectInfo,
+  ReplaceService,
+  AllowMultiple,
+  EventKey,
+  IEventHandler,
+  EventHandler,
   IController,
   Controller,
   IService,
   Service,
   ISettingManager,
   SettingManager,
-  HttpRequest,
+  IQueueManager,
+  QueueManager,
+  IQueueManagerFactory,
+  QueueManagerFactory,
   HttpGet,
-  HttpDelete,
-  HttpPut,
-  HttpOptions,
   HttpPost,
+  HttpPut,
+  HttpDelete,
+  HttpOptions,
+  HttpRequest,
   Router,
   RequestBody,
   RequestQuery,
+  ArrayHelper,
 };
 
 export default Program;
